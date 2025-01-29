4c9e62b890ec4631a8a163175cee97c010e96a96
Swati Saxena <swati.lpu17@gmail.com>
2025-01-28 19:16:19 -0800

    fixed cors issue and updated port

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"SafeShotner-Services/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	//initialize a fiber application
 	app := fiber.New()
+
+	// Enable CORS for all routes
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+	}))
 
 	//Run database
 	configs.ConnectDB()
@@ -20,5 +27,5 @@ func main() {
 	// app.Get("/", func(c *fiber.Ctx) error {
 	// 	return c.JSON(&fiber.Map{"data": "Hello from fiber & mongoDB"})
 	// })
-	app.Listen(":6000")
+	app.Listen(":5000")
 }
